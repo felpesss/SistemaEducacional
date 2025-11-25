e42df29a69872d3991fc37b30b66e86eac0a9159
gabriel alberto duarte de andrade <166182008+Gabrielalbertodu@users.noreply.github.com>
2025-11-25 16:42:42 -0300

    Delete src directory

--- src/main/java/com/sistema/educacional/repository/LogAuditoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.educacional.repository;
-
-import com.sistema.educacional.model.LogAuditoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogAuditoriaRepository extends JpaRepository<LogAuditoria, String> {
-}
